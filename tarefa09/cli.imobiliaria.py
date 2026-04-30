17c1877b60117c4edda56a5ffbeebe3a40fdabb6
luizfelipegit <felipesilva253545@gmail.com>
2026-04-30 17:12:02 -0300

    tarefa09

---
+++ tarefa09/cli.imobiliaria.py
@@ -0,0 +1,66 @@
+{
+  "imobiliaria": {
+    "imoveis": [
+      {
+        "descricao": "Casa moderna com 3 quartos e area gourmet",
+        "valor": 350000,
+        "proprietario": {
+          "nome": "Carlos Silva",
+          "email": "carlos.silva@email.com",
+          "telefones": ["84999990000"]
+        },
+        "endereco": {
+          "rua": "Rua das Flores",
+          "bairro": "Centro",
+          "cidade": "Mossoro",
+          "numero": 120
+        },
+        "caracteristicas": {
+          "tamanho": "120m2",
+          "numQuartos": 3,
+          "numBanheiros": 2
+        }
+      },
+      {
+        "descricao": "Apartamento simples e bem localizado",
+        "valor": 180000,
+        "proprietario": {
+          "nome": "Ana Souza",
+          "email": "",
+          "telefones": []
+        },
+        "endereco": {
+          "rua": "Av. Rio Branco",
+          "bairro": "Alto de Sao Manoel",
+          "cidade": "Mossoro",
+          "numero": null
+        },
+        "caracteristicas": {
+          "tamanho": "70m2",
+          "numQuartos": 2,
+          "numBanheiros": 1
+        }
+      },
+      {
+        "descricao": "Casa grande com piscina e garagem ampla",
+        "valor": 500000,
+        "proprietario": {
+          "nome": "Fernanda Lima",
+          "email": "fernanda@email.com",
+          "telefones": ["84988887777", "84977776666"]
+        },
+        "endereco": {
+          "rua": "Rua Joao da Escossia",
+          "bairro": "Nova Betania",
+          "cidade": "Mossoro",
+          "numero": 450
+        },
+        "caracteristicas": {
+          "tamanho": "200m2",
+          "numQuartos": 4,
+          "numBanheiros": 3
+        }
+      }
+    ]
+  }
+}
